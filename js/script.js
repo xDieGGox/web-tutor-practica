1a6b640a6e684569418f6c3e43a8024a5736bf04
Diego Loja <diegoloja85@gmail.com>
2026-03-16 21:00:16 -0500

    Agregación de efectos a imagenes

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ const navMenu = document.getElementById("navMenu");
 burgerBtn.addEventListener("click", function () {
     navMenu.classList.toggle("header__nav--active");
 });
+
+
+const revealElements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("reveal--active");
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+revealElements.forEach((element) => {
+    observer.observe(element);
+});
