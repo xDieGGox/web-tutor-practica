1e9264a2eb4537eaac2bd67aaf56f3906dbb0e0c
Diego Loja <diegoloja85@gmail.com>
2026-03-19 15:30:47 -0500

    Correccion de acciones para las acciones de los botones y del logo

--- js/script.js
+++ js/script.js
@@ -21,3 +21,31 @@ const observer = new IntersectionObserver((entries) => {
 revealElements.forEach((element) => {
     observer.observe(element);
 });
+
+
+
+const modal = document.getElementById("contactModal");
+const openModalButtons = document.querySelectorAll(".open-modal");
+const closeModal = document.getElementById("closeModal");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modalOverlay = document.getElementById("modalOverlay");
+
+openModalButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+        modal.classList.add("modal--show");
+    });
+});
+
+function hideModal() {
+    modal.classList.remove("modal--show");
+}
+
+closeModal.addEventListener("click", hideModal);
+closeModalBtn.addEventListener("click", hideModal);
+modalOverlay.addEventListener("click", hideModal);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+        hideModal();
+    }
+});
